97ba70cf3ce408c9ba2cb6ae3018e3cbc1fc3074
quickstraw <nathanmcgraw64@gmail.com>
2023-07-24 11:03:13 -0500

    Changed m_rxCurByte to uint16_t.

--- src/SoftwareSerial.h
+++ src/SoftwareSerial.h
@@ -353,7 +353,7 @@ private:
     uint8_t m_parityInPos;
     uint8_t m_parityOutPos;
     int8_t m_rxLastBit; // 0 thru (m_pduBits - m_stopBits - 1): data/parity bits. -1: start bit. (m_pduBits - 1): stop bit.
-    uint8_t m_rxCurByte = 0;
+    uint16_t m_rxCurByte = 0;
     std::unique_ptr<circular_queue<uint8_t> > m_buffer;
     std::unique_ptr<circular_queue<uint8_t> > m_parityBuffer;
     uint32_t m_periodStart;
